143bb462cf82d61de5b80780d6210f85d2359f7e
Zenati <tarek@MacBook-Pro-de-Zenati.local>
2023-11-29 10:24:55 +0100

    firt push

--- imputation_tests.py
+++ imputation_tests.py
@@ -3,7 +3,8 @@ import dgl
 import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score
-#from sklearn.experimental import IterativeImputer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 from stanscofi.datasets import Dataset
 from stanscofi.training_testing import random_simple_split
@@ -26,23 +27,37 @@ PARAMS = {
 class HANBench(HAN):
     def __init__(self, params=None):
         super().__init__(params)
-        self.name = "HAN1" 
+        self.name = "HAN1"
 
     def preprocessing(self, dataset, inf=2, is_training=True):
-        ################### TO MODIFY (BEGIN)
-        # if (self.scalerS is None):
-        #     self.scalerS = IterativeImputer(max_iter=3, random_state=SEED)
-        # if (self.scalerP is None):
-        #     self.scalerP = IterativeImputer(max_iter=3, random_state=SEED)
-        ################### TO MODIFY (END) 
-        # drug_drug = self.scalerS.fit_transform(dataset.items.T.toarray().copy())
-        # disease_disease = self.scalerP.fit_transform(dataset.users.T.toarray().copy())
+        ################## TO MODIFY (BEGIN)
+        if (self.scalerS is None):
+            self.scalerS = IterativeImputer(max_iter=1, random_state=SEED)
+        if (self.scalerP is None):
+            self.scalerP = IterativeImputer(max_iter=1, random_state=SEED)
+        ################## TO MODIFY (END)
+
+        item_features = dataset.items.T.toarray().copy()
+        uses_features = dataset.users.T.toarray().copy()
+
+        item_features_var = item_features.var(axis=0).sort_values(ascending=False)
+        item_features[item_features==0] = np.nan
+        thres=item_features_var[50]
+        item_features[item_features.abs()<thres] = np.nan
+        item_features = item_features.dropna(how="all")
+
+        print('Preprocessing starts...')
+        print('Missing value imputation starts...')
+
+        drug_drug = self.scalerS.fit_transform()
+        disease_disease = self.scalerP.fit_transform()
+        print('Missing value imputation finished!')
 
         drug_drug = dataset.items.T.toarray().copy()
         disease_disease = dataset.users.T.toarray().copy()
 
         #drug_drug = np.nan_to_num(drug_drug, nan=0) ##
-        #disease_disease = np.nan_to_num(disease_disease, nan=0) ##      
+        #disease_disease = np.nan_to_num(disease_disease, nan=0) ##
 
         drug_drug = drug_drug if (drug_drug.shape[0]==drug_drug.shape[1]) else np.corrcoef(drug_drug)
         disease_disease = disease_disease if (disease_disease.shape[0]==disease_disease.shape[1]) else np.corrcoef(disease_disease)
@@ -68,6 +83,7 @@ class HANBench(HAN):
         data = torch.tensor(np.column_stack((dataset.folds.row, dataset.folds.col)).astype('int64')).to(self.device)
         label = torch.tensor(drug_disease[dataset.folds.row, dataset.folds.col].flatten()).float().to(self.device)
         shp = dataset.ratings.shape
+        print('Preprocessing finished!')
         return [g, data, label, shp] if (is_training) else [g]
 
 
@@ -79,16 +95,15 @@ def get_dataset(folder):
 
 
 def main():
-	
-	dataset = get_dataset(folder=DATA_FOLDER)
 
-	train_folds, test_folds, _ = random_simple_split(dataset, 0.20)
+	dataset = get_dataset(folder=DATA_FOLDER)
+	(train_folds, test_folds), _ = random_simple_split(dataset, 0.20)
 	train_dataset = dataset.subset(train_folds, subset_name="Train")
 	test_dataset = dataset.subset(test_folds, subset_name="Test")
 	train_dataset.summary()
 	test_dataset.summary()
 
-	model = HAN(PARAMS)
+	model = HANBench(PARAMS)
 	model.fit(train_dataset, seed=SEED)  # single fit
 
 	scores = model.predict_proba(test_dataset)
